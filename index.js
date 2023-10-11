9a638ff1d84830b5ff48245eb0442e5a03f8c6de
Your Name <you@example.com>
2023-10-11 14:02:07 +0200

    pierwszyEtap

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const mongoString = "mongodb+srv://garnuch:<garnuch123>@xavigarnuch.rldhrtz.mongodb.net/?retryWrites=true&w=majority";
+const mongoString = "mongodb+srv://garnuch:garnuch123@xavigarnuch.rldhrtz.mongodb.net/?retryWrites=true&w=majority";
 
 const {MongoClient} = require('mongodb');
 
 const client = new MongoClient(mongoString);
 
 async function main() {
+    try {
 
-    await client.connect();
+        await client.connect();
 
-    await client.close();
+        await listDB(client);
+
+    } catch (error) {
+
+        console.error(error)
+
+    } finally {
+
+        await client.close();
+
+    }
+}
+
+async function listDB(client) {
+
+    let databaseList = await client.db().admin().listDatabases();
+    
+    console.log("Lista baz danych: "); 
+
+    databaseList.databases.forEach(database =>{
+        console.log("Nazwa: " + database.name + ", Rozmiar: " + database.sizeOnDisk);
+    });
 
 }
+
 main();
